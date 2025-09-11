aeb869918cf9a75e0e4381255fe8a646936be524
Ansuman11055 <ansuman11c5@gmail.com>
2025-09-12 03:25:07 +0530

    bug fixes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css';  // This line is causing the error
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,16 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// Register service worker for PWA
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then((registration) => {
-        console.log('SW registered: ', registration);
-      })
-      .catch((registrationError) => {
-        console.log('SW registration failed: ', registrationError);
-      });
-  });
-}
